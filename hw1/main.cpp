5b0254df8f59ffeea13d88c0c2b3afeb16f9503e
Berker ACIR <berkeracir159@gmail.com>
2018-10-21 14:16:44 +0300

    Add main.cpp

---
+++ hw1/main.cpp
@@ -0,0 +1,48 @@
+#include "support_files/parser.h"
+#include "support_files/ppm.h"
+#include <iostream>
+
+typedef unsigned char RGB[3];
+
+int main(int argc, char* argv[]) {
+    parser::Scene scene;
+
+    scene.loadFromXml(argv[1]);
+
+    parser::Vec3f vec3f = {0.1, 0.2, 0.3};
+
+    std::cout << scene << std::endl;
+
+    /*
+    const RGB BAR_COLOR[8] =
+    {
+        { 255, 255, 255 },  // 100% White
+        { 255, 255,   0 },  // Yellow
+        {   0, 255, 255 },  // Cyan
+        {   0, 255,   0 },  // Green
+        { 255,   0, 255 },  // Magenta
+        { 255,   0,   0 },  // Red
+        {   0,   0, 255 },  // Blue
+        {   0,   0,   0 },  // Black
+    };
+
+    int width = 640, height = 480;
+    int columnWidth = width / 8;
+
+    unsigned char* image = new unsigned char [width * height * 3];
+
+    int i = 0;
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            int colIdx = x / columnWidth;
+            image[i++] = BAR_COLOR[colIdx][0];
+            image[i++] = BAR_COLOR[colIdx][1];
+            image[i++] = BAR_COLOR[colIdx][2];
+        }
+    }
+
+    write_ppm(argv[2], image, width, height);
+    */
+}
