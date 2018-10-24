ec028fb0beb8f8f26027d300709cba21f4e026ce
Berker ACIR <berkeracir159@gmail.com>
2018-10-24 15:12:35 +0300

    Implementation of Ray - Sphere Intersection Function

--- hw1/support_files/ray_trace.cpp
+++ hw1/support_files/ray_trace.cpp
@@ -31,7 +31,6 @@ template parser::Vec3f vector_scalar<int>(const parser::Vec3f &, const int &);
 template parser::Vec3f vector_scalar<float>(const parser::Vec3f &, const float &);
 template parser::Vec3f vector_scalar<double>(const parser::Vec3f &, const double &);
 
-
 float vector_dot(const parser::Vec3f &lhs, const parser::Vec3f &rhs) {
     return lhs.x*rhs.x + lhs.y*rhs.y + lhs.z*rhs.z;
 }
@@ -40,8 +39,7 @@ parser::Vec3f vector_cross(const parser::Vec3f &lhs, const parser::Vec3f &rhs) {
     return parser::Vec3f {lhs.y*rhs.z-lhs.z*rhs.y, lhs.z*rhs.x-lhs.x*rhs.z, lhs.x*rhs.y-lhs.y*rhs.x};
 }
 
-
-bool ray_sphere_intersection(const parser::Vec3f &o, const parser::Vec3f &d, const parser::Vec3f &c, const float &r) {
+bool is_ray_sphere_intersect(const parser::Vec3f &o, const parser::Vec3f &d, const parser::Vec3f &c, const float &r) {
     float A = vector_dot(d, d);
     float B = 2*vector_dot(d, o-c);
     float C = vector_dot(o-c, o-c) - r*r;
@@ -51,4 +49,25 @@ bool ray_sphere_intersection(const parser::Vec3f &o, const parser::Vec3f &d, con
     }
     else
         return 0;
+}
+
+float ray_sphere_intersection(const parser::Vec3f &o, const parser::Vec3f &d, const parser::Vec3f &c, const float &r) {
+    float A = vector_dot(d, d);
+    float B = 2*vector_dot(d, o-c);
+    float C = vector_dot(o-c, o-c) - r*r;
+    float delta = B*B-4*A*C;
+
+    if (delta >= 0) {
+        float t1 = (-B + sqrt(delta))/(2*A);
+        float t2 = (-B - sqrt(delta))/(2*A);
+
+        float result = std::min(std::max((float) 0, t1), std::max((float)0, t2));
+
+        if (result > 0)
+            return result;
+        else
+            return -1;
+    }
+    else
+        return -1;
 }
--- hw1/support_files/ray_trace.h
+++ hw1/support_files/ray_trace.h
@@ -35,15 +35,27 @@ parser::Vec3f vector_cross(const parser::Vec3f &lhs, const parser::Vec3f &rhs);
 
 // Check whether ray and sphere intersects
 // Inputs: Vec3f o, Vec3f d, Vec3f c, float r
-// r(t): o + t.d (o: origin, d: vector)
-// Sphere: (P-c)*(P-c) = r*r (P: point on sphere, c: coordinate of center, r: radius)
-// Solve the equation for t: (Note that . is dot product)
-//      (d.d)*t*t + 2*d.(o-c)*t + (o-c).(o-c) - r*r = 0
+//      r(t): o + t.d (o: origin, d: vector)
+//      Sphere: (P-c)*(P-c) = r*r (P: point on sphere, c: coordinate of center, r: radius)
+//      Solve the equation for t: (Note that . is dot product)
+//          (d.d)*t*t + 2*d.(o-c)*t + (o-c).(o-c) - r*r = 0
 // Output: 1 (true) if ray and sphere intersects (B*B-4*A*C >= 0), 0 (false) otherwise
 //      A = d.d, B = 2*d.(o-c) and C = (o-c).(o-c) - r*r
 //      t1 = (-B + sqrt(B*B-4*A*C))/(2*A)
 //      t2 = (-B - sqrt(B*B-4*A*C))/(2*A)
-bool ray_sphere_intersection(const parser::Vec3f &o, const parser::Vec3f &d, const parser::Vec3f &c, const float &r);
+bool is_ray_sphere_intersect(const parser::Vec3f &o, const parser::Vec3f &d, const parser::Vec3f &c, const float &r);
+
+// t value for Ray and Sphere Intersection
+// Inputs: Vec3f o, Vec3f d, Vec3f c, float r
+//      r(t): o + t.d (o: origin, d: vector)
+//      Sphere: (P-c)*(P-c) = r*r (P: point on sphere, c: coordinate of center, r: radius)
+//      Solve the equation for t: (Note that . is dot product)
+//          (d.d)*t*t + 2*d.(o-c)*t + (o-c).(o-c) - r*r = 0
+// Output: Return the smallest positive t value, if ray and sphere don't intersect, return -1
+//      A = d.d, B = 2*d.(o-c) and C = (o-c).(o-c) - r*r
+//      t1 = (-B + sqrt(B*B-4*A*C))/(2*A)
+//      t2 = (-B - sqrt(B*B-4*A*C))/(2*A)
+float ray_sphere_intersection(const parser::Vec3f &o, const parser::Vec3f &d, const parser::Vec3f &c, const float &r);
 
 
 #endif
