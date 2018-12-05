88229bb645ed95049f005b146baa94c3c2f75cae
Berker ACIR <berkeracir159@gmail.com>
2018-12-05 13:25:46 +0300

    reinitialize

--- hw2/source/rasterizer.cpp
+++ hw2/source/rasterizer.cpp
@@ -36,8 +36,6 @@ int backfaceCullingSetting = 0;
 
 Color **image;
 
-
-
 /*
 	Initializes image with background color
 */
@@ -53,71 +51,14 @@ void initializeImage(Camera cam) {
         }
 }
 
-
-/*
-Gets two parameters, first one transformation matrix
-second one is the point that needed to be transformed
-It return new point after transformation.
-*/
-Vec3 transfromPoint(double transformMatrix[4][4], const Vec3 &point){
-    Vec3 newPoint;
-    double pointMatrix[4] = {point.x, point.y, point.z, 1};
-    double resultMatrix[4];
-    multiplyMatrixWithVec4d(resultMatrix, transformMatrix, pointMatrix);
-    newPoint.x = resultMatrix[0];
-    newPoint.y = resultMatrix[1];
-    newPoint.z = resultMatrix[2];
-    return newPoint;
-}
-
-/*
- * Multiply matrices m1 (double[3][4]) and m2 (double[4][4]) and store the result in result matrix r (double[3][4]).
- */
-void multiplyMWM(double r[3][4], double m1[3][4], double m2[4][4]) {
-    int i, j, k;
-    double total;
-
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 4; j++) {
-            total = 0;
-            for (k = 0; k < 4; k++)
-                total += m1[i][k] * m2[k][j];
-            r[i][j] = total;
-        }
-}
-
-/*
-This function gets camera and result matrix which is empty for initial
-After calculating last transformation matrix, it stores the result in resultMatrix*/
-void getM_l2w(Camera cam, double resultMatrix[3][4]){
-    //This is camera transformation matrix
-    double M_cam[4][4] = {
-        {cam.u.x, cam.u.y, cam.u.z, -(cam.u.x * cam.pos.x + cam.u.y * cam.pos.y + cam.u.z * cam.pos.z)},
-        {cam.v.x, cam.v.y, cam.v.z, -(cam.v.x * cam.pos.x + cam.v.y * cam.pos.y + cam.v.z * cam.pos.z)},
-        {cam.w.x, cam.w.y, cam.w.z, -(cam.w.x * cam.pos.x + cam.w.y * cam.pos.y + cam.w.z * cam.pos.z)},
-        {0, 0, 0, 1}
-    };
-    //This is perspective transformation matrix
-    double M_per[4][4] = {
-        {(2 * cam.n) / (cam.r - cam.l), 0, (cam.r + cam.l) / (cam.r - cam.l), 0},
-        {0, (2 * cam.n) / (cam.t - cam.b), (cam.t + cam.b) / (cam.t - cam.b), 0},
-        {0, 0, -(cam.f + cam.n) / (cam.f - cam.n), -(2 * cam.f * cam.n) / (cam.f - cam.n)},
-        {0, 0, -1, 0}
-    };
-    //This is viewport transformation matrix
-    double M_vp[3][4] = {
-        {cam.sizeX / 2, 0, 0, (cam.sizeX -1) / 2},
-        {0, cam.sizeY / 2, 0, (cam.sizeY -1) / 2},
-        {0, 0, 0.5, 0.5}
-    };
-
-    double M_per_cam[4][4];
-    // M_vp * M_per * M_cam = Transform matrix which can transform vertices to the viewing volume
-    multiplyMatrixWithMatrix;(M_per_cam, M_per, M_cam);
-    multiplyMWM(resultMatrix, M_vp, M_per_cam);
-}
-
-
+void viewport_transformation(double result[4][4], Camera cam);
+void perspective_transformation(double result[4][4], Camera cam);
+void camera_transformation(double result[4][4], Camera cam);
+void local_to_world(double result[3][4], double M_vp[3][4], double M_per[4][4], double M_cam[4][4]);
+void translate(double M_translate[4][4], Translation t);
+void scale(double M_scale[4][4], Scaling s);
+void rotate(double M_rotate[4][4], Rotation r);
+void model_transformation(double M_model[4][4], int model_id);
 
 /*
 	Transformations, culling, rasterization are done here.
@@ -125,15 +66,35 @@ void getM_l2w(Camera cam, double resultMatrix[3][4]){
 	Using types in "hw2_types.h" and functions in "hw2_math_ops.cpp" will speed you up while working.
 */
 void forwardRenderingPipeline(Camera cam) {
-    double M_l2w[3][4];  
-    getM_l2w(cam, M_l2w);
+    double M_vp[3][4];
+    viewport_transformation(M_vp, cam);
+    double M_per[4][4];
+    perspective_transformation(M_per, cam);
+    double M_cam[4][4];
+    camera_transformation(M_cam, cam);
 
-    
+    double M_l2w[3][4];
+    local_to_world(M_l2w, M_vp, M_per, M_cam);
+
+    Vec3 world_vertices[100000];
+
+    for (int i=0; i<numberOfModels; i++) {
+        double M_model[4][4];
+        model_transformation(M_model, i);
+        // M_l2w = M_l2w * M_model; TODO
+
+        for (int j=0; j<models[i].numberOfTriangles; j++) {
+            models[i].triangles[j].vertexIds[0];
+            models[i].triangles[j].vertexIds[1];
+            models[i].triangles[j].vertexIds[2];
+        }
+    }
 }
 
 
 int main(int argc, char **argv) {
     int i, j;
+
     if (argc < 2) {
         std::cout << "Usage: ./rasterizer <scene file> <camera file>" << std::endl;
         return 1;
@@ -184,7 +145,7 @@ int main(int argc, char **argv) {
         // Converts PPM image in given path to PNG file, by calling ImageMagick's 'convert' command.
         // Notice that os_type is not given as 1 (Ubuntu) or 2 (Windows), below call doesn't do conversion.
         // Change os_type to 1 or 2, after being sure that you have ImageMagick installed.
-        convertPPMToPNG(cameras[i].outputFileName, 99);
+        convertPPMToPNG(cameras[i].outputFileName, 1);
     }
 
     return 0;
